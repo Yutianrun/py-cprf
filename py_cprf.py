1cd2d0929147a03b8b428e734269801dcd50556a
yu <349326158@qq.com>
2024-12-22 09:03:41 +0000

    arbitray random policy

--- py_cprf.py
+++ py_cprf.py
@@ -21,7 +21,8 @@ class cPRF:
         self.pp = None
 
     def encode_vt(self, a):
-        return str(bin(a[0][0]))[2:].zfill(self.log_t) + str(bin(a[0][1]))[2:].zfill(self.log_t) + a[1]
+        print(a)
+        return "".join([bin(i)[2:].zfill(self.log_t) for i in a[0]]) + a[1]
 
     def aes_prf(self, input_bits, key):
         # AES计算逻辑
@@ -30,31 +31,17 @@ class cPRF:
         return encrypted
     
     def lambda_bit_prf(self, input_bits, key_as_seed):
-
-        # xor version to simplify the circuit
+        
         if len(input_bits) > self.lambda_ or any(bit not in '01' for bit in input_bits):
             raise ValueError(f'input_bits 必须是小于{self.lambda_}位的比特字符串, 现有字符串位数为{len(input_bits)}')
         elif len(input_bits) < self.lambda_:
             input_bits = input_bits.zfill(self.lambda_)
-        
         random.seed(key_as_seed)
-        random_bits = ''.join(random.choice('01') for _ in range(self.lambda_))
-        
-        prf_result = ''.join(str(int(input_bits[i]) ^ int(random_bits[i])) for i in range(self.lambda_))
-        
-        return prf_result
-        
-        # prp version to instantile the prf
-        # if len(input_bits) > self.lambda_ or any(bit not in '01' for bit in input_bits):
-        #     raise ValueError(f'input_bits 必须是小于{self.lambda_}位的比特字符串, 现有字符串位数为{len(input_bits)}')
-        # elif len(input_bits) < self.lambda_:
-        #     input_bits = input_bits.zfill(self.lambda_)
-        # random.seed(key_as_seed)
-        # keys = [''.join(bits) for bits in itertools.product('01', repeat=self.lambda_)]
-        # values = keys.copy()
-        # random.shuffle(values)
-        # prf_table = dict(zip(keys, values))
-        # return prf_table.get(input_bits, '0' * self.lambda_)
+        keys = [''.join(bits) for bits in itertools.product('01', repeat=self.lambda_)]
+        values = keys.copy()
+        random.shuffle(values)
+        prf_table = dict(zip(keys, values))
+        return prf_table.get(input_bits, '0' * self.lambda_)
 
 
     def Setup(self):
@@ -190,9 +177,9 @@ class cPRF:
 # 使用示例
 if __name__ == "__main__":
 
-    t = 2  # 子集的大小
-    l = 4  # 输入长度
-    lambda_ = 6  # AES密钥长度
+    t = 1  # 子集的大小
+    l = 2  # 输入长度
+    lambda_ = 4  # AES密钥长度
     
     # lambda_ = t * (l-1).bit_length() + t  # AES密钥长度
     cprf = cPRF(t, l, lambda_=lambda_)
@@ -202,11 +189,33 @@ if __name__ == "__main__":
     # print("Master Secret Key (msk):", msk.hex())
     
     # 定义策略 f
+    # f = [
+    #     ((0, 1), lambda v: v == "10"),  # 子句1：索引子集为(0, 1)，位串为"10"时满足条件
+    #     ((2, 3), lambda v: v != "01"),  # 子句2：索引子集为(2, 3)，位串为"01"时满足条件
+    #     ((1, 2), lambda v: v in ["01", "00"])  # 子句3：索引子集为(1, 2)，位串为"01"或"00"时满足条件
+    # ]
+
     f = [
-        ((0, 1), lambda v: v == "10"),  # 子句1：索引子集为(0, 1)，位串为"10"时满足条件
-        ((2, 3), lambda v: v != "01"),  # 子句2：索引子集为(2, 3)，位串为"01"时满足条件
-        ((1, 2), lambda v: v in ["01", "00"])  # 子句3：索引子集为(1, 2)，位串为"01"或"00"时满足条件
+        ((0,), lambda v: v == "1"),  # 子句1：索引子集为(0, 1)，位串为"10"时满足条件
+        # ((1), lambda v: v == "1")
     ]
+    # 随机生成一些子句
+    # print(f)
+    # k = 1
+    # condition_str =[''.join([str(random.randint(0, 1)) for _ in range(t)]) for _ in range(k)]
+    # print(condition_str)
+    # f = [(tuple(random.sample(range(l), t)), lambda v: v == condition_str[i])  for i in range(k)]
+
+    # print(f)
+  # 固定随机种子以便复现结果
+    # num_clauses = 2  # 生成的子句数量
+    # f = []
+    # for _ in range(num_clauses):
+    #     Ti = tuple(random.sample(range(l), t))
+    #     fi = lambda v: random.choice([0, 1])
+    #     f.append((Ti, fi))
+
+    # print("策略 f:", f)
     
     # 生成受限密钥
     sk_f = cprf.Constrain(f)
@@ -214,15 +223,22 @@ if __name__ == "__main__":
     # 多个 x 测试
     # test_xs = ["1010","1111"]
     # test_xs = ["1010", "1111", "0000", "0101", "0011"]
-    test_xs = ["0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"]
-    
+    # test_xs = ["0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"]
+    test_xs = [''.join(bits) for bits in itertools.product('01', repeat=l)]
+    #
     for x in test_xs:
         # 检查 x 是否满足策略
         S_x = cprf._get_subsets(x)
+        print("s_x", S_x)
         satisfies_policy = True
         for Ti, fi in f:
+            print("Ti, fi", Ti, fi)
             subset_v = next((v for T, v in S_x if T == Ti), None)
+            print("subset_v", subset_v)
+            print("subset_v is None", subset_v is None)
+            print("not fi(subset_v)", not fi(subset_v))
             if subset_v is None or not fi(subset_v):
+
                 satisfies_policy = False
                 break
         
